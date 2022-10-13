8e67096bcef0e4cab2f825c056fe93996673436b
Diego-Batista <diegobtistadev@gmail.com>
2022-10-13 17:07:48 -0300

    finalizando component Text

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        xs: 14,
+        sm: 16,
+        md: 18,
+        lg: 20,
+        xl: 24,
+        '2xl': 32,
+      },
+      colors: {
+        'gray-900': '#121214',
+        'gray-800': '#202024',
+        'gray-400': '#7C7C8A',
+        'gray-200': '#C4C4CC',
+        'gray-100': '#E1E1E6',
+
+        'brand-500': '#8257E6',
+        'brand-300': '#9D73FF',
+      },
       fontFamily: {
         sans: 'Inter, sans-serif'
       }
